002b447a18d3b48717b861a5d869dd7f79b94b3e
DevDas <55701713+DevDas@users.noreply.github.com>
2020-08-31 19:59:59 +0530

    Fixed Clamp Issue

--- BlockBreaker/Assets/Scripts/Paddle.cs
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float ScreenWidthInUnits = 16f;
+    [SerializeField] float MinX = 0;
+    [SerializeField] float MaxX = 17.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,6 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float MousePositionX = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
-        transform.position = new Vector2(Mathf.Clamp(MousePositionX, 0, ScreenWidthInUnits), transform.position.y);
+        transform.position = new Vector2(Mathf.Clamp(MousePositionX, MinX, MaxX), transform.position.y);
     }
 }
