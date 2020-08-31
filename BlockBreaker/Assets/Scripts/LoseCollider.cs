01c95398b2be4301bc72b4bf68e5916632e2f2fd
DevDas <55701713+DevDas@users.noreply.github.com>
2020-08-31 19:46:03 +0530

    Move Object With Mouse

--- BlockBreaker/Assets/Scripts/LoseCollider.cs
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -22,6 +22,5 @@ public class LoseCollider : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene("GameOver");
-    }
-
+    } 
 }
---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float ScreenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MousePositionX = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        transform.position = new Vector2(Mathf.Clamp(MousePositionX, 0, ScreenWidthInUnits), transform.position.y);
+    }
+}
