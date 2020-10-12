490cf7e3892eb87836db18122eac4ff74725cade
DevDas <55701713+DevDas@users.noreply.github.com>
2020-10-12 21:35:22 +0530

    Win The Level And Load Alternate Method

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -8,25 +8,18 @@ public class Block : MonoBehaviour
 
     // Cached Reference
     Level LevelObject;
-    SceneLoader sceneLoader;
 
     private void Start()
     {
         LevelObject = FindObjectOfType<Level>();
         LevelObject.CountBreakableBlocks();
-        sceneLoader = FindObjectOfType<SceneLoader>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(BreakSound, Camera.main.transform.position);
 
-        --LevelObject.BreakableBlocks;
-
-        if (LevelObject.BreakableBlocks <= 0)
-        {
-            sceneLoader.LoadNextScene();
-        }
+        LevelObject.BlockDestroyed();
 
         Destroy(gameObject);
     }
--- BlockBreaker/Assets/Scripts/Level.cs
+++ BlockBreaker/Assets/Scripts/Level.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     [SerializeField] public int BreakableBlocks;
-    
+
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         BreakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        --BreakableBlocks;
+
+        if (BreakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
