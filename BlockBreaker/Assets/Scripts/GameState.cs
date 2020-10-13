3fdec88f978f6f219b488e8219013e0d81f3c84a
DevDas <55701713+DevDas@users.noreply.github.com>
2020-10-13 14:33:14 +0530

    Using Time.timeScale

---
+++ BlockBreaker/Assets/Scripts/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] float GameSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Time.timeScale = GameSpeed;
+    }
+}
